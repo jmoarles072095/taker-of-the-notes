ef97e14b8aac10a500d45354653e81c7b54be421
Jose Morales <jmorales072095@gmail.com>
2020-10-09 14:16:38 -0700

    updated files and edited code

--- Routes/apiRoute.js
+++ Routes/apiRoute.js
@@ -4,13 +4,11 @@ const express = require("express");
 const notesData = require("../data/db.json");
 
 module.exports = function(app) {
+
     function writeToDB(notes) {
-
         // Converts the JSON array back into a string
-
         notes = JSON.stringify(notes);
         console.log(notes);
-        // Writes String back to db.json
         fs.writeFileSync("./data/db.json", notes, function(err) {
             if (err) {
                 return console.log(err);
@@ -18,10 +16,12 @@ module.exports = function(app) {
         });
     }
 
+    // GET Method to return all the notes
     app.get("/api/notes", function(req, res) {
         res.json(notesData);
     });
 
+    // POST Method to add the notes
     app.post("/api/notes", function(req, res) {
 
         if (notesData.length == 0) {
@@ -32,29 +32,29 @@ module.exports = function(app) {
 
         console.log("req.body.id: " + req.body.id);
 
-        // Pushes body to the JSON array
         notesData.push(req.body);
 
-        // saves notes/ writes them in the database
+        // Write notes data to the database
         writeToDB(notesData);
         console.log(notesData);
 
-        // returns the new note in JSON format
+        // returns new note in JSON format.
         res.json(req.body);
     });
 
-    // DELETE Method helps delete note with specified ID
+    // DELETE Method to delete note with specified ID
     app.delete("/api/notes/:id", function(req, res) {
 
         // Obtains id and converts to a string
         let id = req.params.id.toString();
         console.log(id);
 
-        // Goes through notes array searching for matching id
+        // Goes through notesArray searching for matching ID
         for (i = 0; i < notesData.length; i++) {
 
             if (notesData[i].id == id) {
                 console.log("match!");
+                // responds with deleted note
                 res.send(notesData[i]);
 
                 // Removes the deleted note
@@ -63,7 +63,7 @@ module.exports = function(app) {
             }
         }
 
-        // Write the notes data to database
+        // Write notes data to database
         writeToDB(notesData);
 
     });
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-// add required modules
+// add required dependencies
+
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -6,15 +7,13 @@ const path = require("path");
 // Create server application at port 3000
 const PORT = process.env.PORT || 3000;
 
-// Read URL or JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Include js files
+
 require("./routes/apiRoute")(app);
 require("./routes/htmlRoute")(app);
 
-// Use public folder
 app.use(express.static("public"));
 
 // Add listener
